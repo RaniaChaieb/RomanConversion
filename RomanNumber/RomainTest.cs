5af04750e8388f61e2238ae4e312e1bb683c6edf
Chaiebrania <rchaieb@naxxum.fr>
2023-11-09 16:25:33 +0100

    romain

---
+++ RomanNumber/RomainTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RomanNumber
+{
+    public class RomainTest
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(10, "X")]
+        [InlineData(7, "VII")]
+        [InlineData(354, "CCCLIV")]
+        [InlineData(3000, "MMM")]
+        public void RomanConversion_WithValidInput_ShouldReturnExpectedValue(int number, string expected)
+        {
+            // Arrange
+
+            // Act
+            string result = Roman.RomanConversion(number);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
---
+++ RomanNumber/Roman.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+public class Roman
+{
+    // Déclaration d'un dictionnaire pour mapper les caractères romains avec leurs valeurs numériques correspondantes
+    private static Dictionary<char, int> ListOfMapping = new Dictionary<char, int>
+    {
+       {'I', 1},
+       {'V', 5},
+       {'X', 10},
+       {'L', 50},
+       {'C', 100},
+       {'D', 500},
+       {'M', 1000}
+    };
+
+    // Fonction pour convertir un nombre en chiffre romain
+    public static string RomanConversion(int number)
+    {
+        var res = "";
+        foreach (var num in ListOfMapping)
+        {
+            // Boucle pour parcourir chaque élément du dictionnaire
+            while (number >= num.Value) // Vérifie si le nombre est supérieur ou égal à la valeur numérique actuelle du caractère romain
+            {
+                res += num.Key; // Ajoute le caractère romain correspondant à la chaîne de résultat
+                number -= num.Value; // Soustrait la valeur numérique du caractère romain du nombre
+            }
+        }
+        return res; // Retourne la chaîne de résultat contenant la conversion du nombre en chiffre romain
+    }
+}
